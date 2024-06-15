f0422736bb7c6d5b7afaeb5192a4b4d733511623
yu-ki2249 <watabeyuki04092002@gmail.com>
2024-06-15 22:05:49 +0900

    nagoyameshiを追加

---
+++ my_nagoyamashi/src/main/java/com/example/nagoyameshi/MyNagoyamashiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagoyameshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNagoyamashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNagoyamashiApplication.class, args);
+	}
+
+}
---
+++ my_nagoyamashi/src/main/resources/schema.sql
@@ -0,0 +1,81 @@
+CREATE TABLE IF NOT EXISTS roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    furigana VARCHAR(50) NOT NULL,
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    birthday DATE,
+    occupation VARCHAR(50),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,    
+    role_id INT NOT NULL, 
+    enabled BOOLEAN NOT NULL,
+    stripe_customer_id VARCHAR(255) UNIQUE,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,    
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+CREATE TABLE IF NOT EXISTS verification_tokens (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL UNIQUE,
+    token VARCHAR(255) NOT NULL,        
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) 
+);
+
+CREATE TABLE IF NOT EXISTS restaurants (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,  
+    image VARCHAR(255),
+    description TEXT NOT NULL,
+    lowest_price INT NOT NULL,
+    highest_price INT NOT NULL,    
+    postal_code VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    opening_time TIME NOT NULL,
+    closing_time TIME NOT NULL,
+    seating_capacity INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS regular_holidays (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    day VARCHAR(50) NOT NULL,  
+    day_index INT
+);
+
+CREATE TABLE IF NOT EXISTS regular_holiday_restaurant (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    restaurant_id INT NOT NULL, 
+    regular_holiday_id INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE (restaurant_id, regular_holiday_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),
+    FOREIGN KEY (regular_holiday_id) REFERENCES regular_holidays (id)       
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL  
+);
+
+CREATE TABLE IF NOT EXISTS category_restaurant (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    restaurant_id INT NOT NULL, 
+    category_id INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE (restaurant_id, category_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)       
+);
